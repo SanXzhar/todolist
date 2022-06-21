a11093cde55c391094f535e8d0a94d44541acbfa
Sanzhar <58833843+SanXzhar@users.noreply.github.com>
2022-06-21 16:39:31 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       method: 'get',
       url: ' https://api.todoist.com/rest/v1/tasks',
       headers: {
-        Authorization: 'Bearer 43ae224e523fa69d5aad6956e5d1df1d62695c2b'}
+        Authorization: 'Bearer $TOKEN'}
     }).then((response) => setItems(response.data))     
   }, [])
   
@@ -32,7 +32,7 @@ function App() {
       headers: {
         'Content-Type': 'application/json',
         'X-Request-Id': uuidv4(),
-        'Authorization': 'Bearer 43ae224e523fa69d5aad6956e5d1df1d62695c2b'
+        'Authorization': 'Bearer $TOKEN'
       } 
     }).then((response) => {
       setItems([...items, response.data]);
@@ -45,7 +45,7 @@ function App() {
       method: 'post',
       url: `https://api.todoist.com/rest/v1/tasks/${id}/close`,
       headers: {
-        Authorization: 'Bearer 43ae224e523fa69d5aad6956e5d1df1d62695c2b' 
+        Authorization: 'Bearer $TOKEN' 
       }
     })
   .then(() => setItems([...items.filter(item => item.id !== id)]))
@@ -56,7 +56,7 @@ function App() {
       method: 'DELETE',
       url: `https://api.todoist.com/rest/v1/tasks/${id}`,
       headers: {
-        Authorization: 'Bearer 43ae224e523fa69d5aad6956e5d1df1d62695c2b'
+        Authorization: 'Bearer $TOKEN'
       }
     })
     .then(() => setItems([...items.filter(item => item.id !== id)]))
